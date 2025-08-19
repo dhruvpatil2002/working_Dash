d9bb3b94cb077e4db8ece67eb780f48a47db4621
dhruv patil <dhruvpatil713@gmail.com>
2025-08-19 11:47:03 +0530

    adding src

--- db/constant.js
+++
@@ -1,1 +0,0 @@
-export const DB_NAME = "AI surveillance DashBoard"
--- db/index.js
+++
@@ -1,7 +0,0 @@
-import dotenv from"dotenv"
-import coonectDB from "./db/index.js";
-import { connect } from "http2";
-dotenv.config({
-    path: './env'
-})
-connectDB()
